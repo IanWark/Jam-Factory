c161d6c680b384cd6ccd20d96ae1211614fab348
fletcher123 <fletcher.st-germain@snowedin.ca>
2025-03-29 12:47:40 -0400

    Pipe Spawner code

--- Assets/Scripts/Pipe.cs
+++ Assets/Scripts/Pipe.cs
@@ -2,15 +2,37 @@ using UnityEngine;
 
 public class Pipe : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject FruitToSpawn;
+    public float SpawnsPerSecond;
+    public float SpawnScaleMin;
+    public float SpawnScaleMax;
+    public float HorizontalForceMin;
+    public float HorizontalForceMax;
+    public float VerticalForceMin;
+    public float VerticalForceMax;
+    public KeyCode debugActivateKey;
+
+    private float TimeSinceLastSpawn = 0.0f;
+    private float WhenToUpdate = 0.0f;
     void Start()
     {
-        
+        WhenToUpdate = SpawnsPerSecond / 60.0f; //??
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(debugActivateKey)) 
+        {
+            TimeSinceLastSpawn += Time.deltaTime;
+            if(TimeSinceLastSpawn > WhenToUpdate) 
+            {
+                GameObject fruit = Instantiate(FruitToSpawn).GetComponent<Fruit>();
+                fruit.Scale = Random.Range(SpawnScaleMin, SpawnScaleMax);
+                fruit.transform.localScale = new Vector3(fruit.Scale, fruit.Scale, fruit.Scale);
+
+                fruit.GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(HorizontalForceMin, HorizontalForceMax), Random.Range(VerticalForceMin, VerticalForceMax)));
+            }
+        }
     }
 }
