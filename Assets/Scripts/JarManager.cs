66107ad64bec15df0664c3f525b235409bf5d4cb
Ian-Wark-Snowedin <wark@ualberta.ca>
2025-03-31 14:35:40 -0400

    Fix finite jars issue

--- Assets/Scripts/JarManager.cs
+++ Assets/Scripts/JarManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class JarManager : MonoBehaviour
@@ -16,17 +17,40 @@ public class JarManager : MonoBehaviour
     [SerializeField]
     private Jar jarPrefab;
 
-    private Jar lastSpawnedJar;
+    private Transform leftMostJarTransform;
+
+    private List<Jar> activeJars;
+    private List<Jar> inactiveJars;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
+        activeJars = new List<Jar>(numJarsToSpawn);
+        inactiveJars = new List<Jar>(numJarsToSpawn);
+
         for (int i = 0; i < numJarsToSpawn; ++i)
         {
             Jar newJar = Instantiate(jarPrefab, GetNewSpawnLocation(), Quaternion.identity);
-            newJar.OnJarDeathEvent += RespawnJar;
+            newJar.OnJarDeathEvent += OnJarDeath;
 
-            lastSpawnedJar = newJar;
+            activeJars.Add(newJar);
+
+            leftMostJarTransform = newJar.transform;
+        }
+    }
+
+    private void Update()
+    {
+        if (inactiveJars.Count > 0
+            && leftMostJarTransform.position.x >= startLocation.position.x)
+        {
+            int index = inactiveJars.Count - 1;
+            Jar jarToRespawn = inactiveJars[index];
+
+            activeJars.Add(jarToRespawn);
+            inactiveJars.RemoveAt(index);
+
+            RespawnJar(jarToRespawn);
         }
     }
 
@@ -36,25 +60,44 @@ public class JarManager : MonoBehaviour
 
         jar.GetComponent<Recipe>().Respawn();
 
-        lastSpawnedJar = jar;
+        leftMostJarTransform = jar.transform;
+
+        jar.gameObject.SetActive(true);
+    }
+
+    private void OnJarDeath(Jar jar)
+    {
+        jar.gameObject.SetActive(false);
+        inactiveJars.Add(jar);
+        activeJars.Remove(jar);
+
+        // Find leftMostJarTransform
+        leftMostJarTransform = startLocation;
+        foreach (Jar activeJar in activeJars)
+        {
+            if (activeJar.transform.position.x < leftMostJarTransform.transform.position.x)
+            {
+                leftMostJarTransform = activeJar.transform;
+            }
+        }
     }
 
     private Vector3 GetNewSpawnLocation()
     {
         Vector3 spawnLocation;
 
-        if (lastSpawnedJar == null)
+        if (leftMostJarTransform == null)
         {
             // Spawn first one forward so we can see it when the game starts
             return startLocation.transform.position;
         }
-        if (lastSpawnedJar.transform.position.x > startLocation.transform.position.x)
+        if (leftMostJarTransform.transform.position.x > startLocation.transform.position.x)
         {
             spawnLocation = startLocation.transform.position;
         }
         else
         {
-            spawnLocation = lastSpawnedJar.transform.position;
+            spawnLocation = leftMostJarTransform.transform.position;
         }
 
         return new Vector3(spawnLocation.x - spaceBetweenJars, spawnLocation.y, spawnLocation.z);
