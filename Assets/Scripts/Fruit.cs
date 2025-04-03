41d30c030f6fee3969003d93d543f3802d165fbe
Ian-Wark-Snowedin <wark@ualberta.ca>
2025-04-02 21:06:59 -0400

    Number of fruit destruction particles based on mass

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class Fruit : MonoBehaviour
 {
+    private const float MAX_FRUIT_PARTICLE_MASS = 0.4f;
+    private const float FRUIT_PARTICLE_RATIO_MULTIPLIER = 200;
+
     public int id;
 
     [SerializeField]
@@ -11,6 +14,8 @@ public class Fruit : MonoBehaviour
     private FruitColourObject particleColour;
 
     private float creationTime;
+
+    private bool beingDestroyed = false;
 
     public GameObject particleSystemPrefab;
 
@@ -26,17 +31,29 @@ public class Fruit : MonoBehaviour
 
     public void DestroyIt()
     {
+        beingDestroyed = true;
+
         SquishSounds.instance.PlaySquish();
 
-        ParticleSystem.MainModule main = Instantiate(particleSystemPrefab, transform.position, transform.rotation).GetComponent<ParticleSystem>().main;
+        Rigidbody2D rigidbody = this.GetComponent<Rigidbody2D>();
+
+        ParticleSystem particleSystem = Instantiate(particleSystemPrefab, transform.position, transform.rotation).GetComponent<ParticleSystem>();
+
+        ParticleSystem.MainModule main = particleSystem.main;
         main.startColor = particleColour.colour;
+
+        float massRatio = rigidbody.mass / MAX_FRUIT_PARTICLE_MASS;
+        Debug.Log($"name: {name} scale: {transform.localScale} mass: {rigidbody.mass} / maxMass {MAX_FRUIT_PARTICLE_MASS} = massRatio {massRatio}");
+        ParticleSystem.EmissionModule emission = particleSystem.emission;
+        emission.rateOverTime = massRatio * FRUIT_PARTICLE_RATIO_MULTIPLIER;
 
         Destroy(gameObject);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if ((collision.gameObject.tag == "Fruit" || collision.gameObject.tag == "Jar")
+        if (!beingDestroyed 
+            && (collision.gameObject.tag == "Fruit" || collision.gameObject.tag == "Jar")
             && IsOld())
         {
             DestroyIt();
